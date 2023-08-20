851d5d5ed1fce688e1d1ea86b985f9610643b124
Curtis O'Dell <curtisodell1@gmail.com>
2023-08-20 15:51:23 -0600

    babies first fib

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    list = []
+    if length > 0 :
+        list.append(0)
+        if length > 1:
+            list.append(1)
+            if length > 2:
+                i = 2
+                while i < length:
+                    list.append(
+                        list[-1] + list[-2])
+                    i += 1
+    print(list)
+
     pass
